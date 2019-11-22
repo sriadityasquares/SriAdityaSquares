51551ee09445aab463a05715c48d11cff5e27356
manojv8 <manojvenkat8@gmail.com>
2019-11-22 15:31:40 -0500

    Check In Correction

---
+++ ModelLayer/Dashboard.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelLayer
+{
+    public class Dashboard
+    {
+        public int Project { get; set; }
+
+        public int Tower { get; set; }
+    }
+}
